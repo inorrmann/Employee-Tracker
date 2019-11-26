aff708a8966013544a0d3d2d17f59cbfea9fc918
Ingrid N <inorrmann@gmail.com>
2019-11-25 21:46:42 -0800

    add department completed

--- employees.js
+++ employees.js
@@ -90,6 +90,7 @@ function prompt() {
 
 
 // *** FORMAT ANSWERS FROM PROMPTS ***
+
 let name;
 // capitalize first letter of answers from prompt
 function upperAnswer(answer) {
@@ -108,20 +109,23 @@ function upperAnswer(answer) {
 // *** VALIDATION FUNCTIONS FOR PROMPTS ***
 
 let previousData;
+let errorMessage;
 
-const roleValidation = (input) => {
+const validation = (input) => {
     upperAnswer(input);
-    
+
     if (input === "") {
         return "Please enter a valid answer";
     }
     else if (previousData.includes(name)) {
-        return "You have already entered this role; please enter a different one.";
+        return errorMessage;
     }
     else {
         return true;
     }
 };
+
+// *** END OF VALIDATION FUNCTIONS FOR PROMPTS ***
 
 
 // finish this
@@ -145,9 +149,8 @@ function addEmp() {
 }
 
 
-
-
 function addRole() {
+    // collect previously saved department to use as choices
     let departments = [];
     connection.query("SELECT name FROM department", function (err, res) {
         if (err) throw err;
@@ -155,6 +158,7 @@ function addRole() {
             departments.push(dept.name);
         });
     });
+    // collect previously entered roles to avoid duplicates (validation)
     connection.query("SELECT title FROM role", function (err, res) {
         if (err) throw err;
         previousData = [];
@@ -162,13 +166,15 @@ function addRole() {
             previousData.push(role.title);
         });
     });
+    // error message for validation process
+    errorMessage = "You have already entered this role; please enter a different one."
+
     inquirer.prompt([
         {
             type: "input",
             message: "What is the name of the role?",
             name: "role",
-            validate: roleValidation
-            // verification of duplicates needs to go here not below!!! similar to email address homeworlk
+            validate: validation
         },
         {
             type: "input",
@@ -186,78 +192,43 @@ function addRole() {
         let salary = res.salary;
         let department = res.department;
 
-        // // convert role to upper case every first letter of words
-        // let role = res.role.trim().toLowerCase().split(" ")
-        // let roleUpper = "";
-        // role.forEach(word => {
-        //     roleUpper += word.charAt(0).toUpperCase() + word.slice(1) + " ";
-        //     roleUpper.trim();
-        // });
-        // let name = roleUpper.trim();
-
-        // compare entered value against previous entries
-        let previousRoles = [];
-        connection.query("SELECT title FROM role", function (err, res) {
+        // get the id from the department selected
+        connection.query("SELECT id FROM department WHERE name=?", [department], function (err, res) {
             if (err) throw err;
-            res.forEach(role => {
-                previousRoles.push(role.title);
+            // save role information to server
+            connection.query(`INSERT INTO role (title, salary, department_id) VALUES ("${name}", ${salary}, ${res[0].id})`, function (err) {
+                if (err) throw err;
+                console.log("Role has been successfully added.")
             });
-
-            if (previousRoles.includes(name)) {
-                console.log("WARNING! You have already entered this role. Please select a different action.");
-            }
-            else {
-                // get the id from the department selected
-                connection.query("SELECT id FROM department WHERE name=?", [department], function (err, res) {
-                    if (err) throw err;
-                    // insert role information
-                    connection.query(`INSERT INTO role (title, salary, department_id) VALUES ("${name}", ${salary}, ${res[0].id})`, function (err) {
-                        if (err) throw err;
-                        console.log("Role has been successfully added.")
-                    });
-                })
-            }
-        });
+        })
         prompt();
     });
 }
 
 
-// *** ADD A DEPARTMENT ***
 function addDepartment() {
+    // collect previously entered departments to avoid duplicates (validation)
+    connection.query("SELECT name FROM department", function (err, res) {
+        if (err) throw err;
+        previousData = [];
+        res.forEach(dept => {
+            previousData.push(dept.name);
+        });
+    });
+    // error message for validation process
+    errorMessage = "You have already entered this department; please enter a different one."
+
     inquirer.prompt([
         {
             type: "input",
             message: "What is the name of the department?",
-            name: "department"
+            name: "department",
+            validate: validation
         }
     ]).then(function (res) {
-        // convert department to upper case every first letter of words
-        let dept = res.department.trim().toLowerCase().split(" ")
-        let deptUpper = "";
-        dept.forEach(word => {
-            deptUpper += word.charAt(0).toUpperCase() + word.slice(1) + " ";
-            deptUpper.trim();
-        });
-        let deptName = deptUpper.trim();
-
-        // compare entered value against previous entries
-        let previousDepts = [];
-        connection.query("SELECT name FROM department", function (err, res) {
+        connection.query(`INSERT INTO department (name) VALUES ("${name}")`, function (err) {
             if (err) throw err;
-            res.forEach(dept => {
-                previousDepts.push(dept.name);
-            });
-
-            if (previousDepts.includes(deptName)) {
-                console.log("WARNING! You have already entered this department. Please select a different action.");
-            }
-            else {
-                connection.query(`INSERT INTO department (name) VALUES ("${deptName}")`, function (err) {
-                    if (err) throw err;
-                    console.log("Deparment has been succesfully added.")
-                });
-            }
+            console.log("Deparment has been succesfully added.")
         });
         prompt();
     });
